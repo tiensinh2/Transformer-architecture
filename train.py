08577e8520f2c34b15bfc191845a98ae4725b9d4
tkiet <smg10vp@gmail.com>
2026-06-04 17:40:59 +0700

    Update train.py

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
 from config import CFG
-from data.dataset import setup_data
+from dataset import setup_data
 from model import Transformer
 
 
